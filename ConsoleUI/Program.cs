b089c8b5b7918480575586321b8b38ed609cdcd6
_shark <stanislav.kozorez@gmail.com>
2016-03-08 18:59:50 +0300

    Task2 done

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,9 +11,64 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Logic Pow: {0} ", Task1.Root(3, 1/20.0));
+            int[][] arr = new int[5][] {new int[] {8, 4, 7, 10, 9, 5, 20, }, null, new int[] { 1, 7, 3 }, null, new int[] { 5, 7, 10, 7 }};
+            int[][] arr2 = new int[arr.Length][];
+
+            Console.WriteLine("Logic Pow: {0} ", Task1.Root(3, 1 / 20.0));
             Console.WriteLine("Math Pow: {0} ", Math.Pow(3, 20));
+
+            Console.WriteLine("Initial array:");
+            arr.CopyTo(arr2, 0);
+            PrintArray(arr2);
+
+            Console.WriteLine("Sorted by max row sum(ascendent):");
+            arr.CopyTo(arr2, 0); 
+            Task2.SortByRowSum(arr2, SortOrder.Ascendent);
+            PrintArray(arr2);
+
+            Console.WriteLine("Sorted by max row sum(descendent):");
+            arr.CopyTo(arr2, 0);
+            Task2.SortByRowSum(arr2, SortOrder.Descendent);
+            PrintArray(arr2);
+
+
+            Console.WriteLine("Sorted by max row element(ascendent):");
+            arr.CopyTo(arr2, 0);
+            Task2.SortByMaxRowElement(arr2, SortOrder.Ascendent);
+            PrintArray(arr2);
+
+            Console.WriteLine("Sorted by max row element(descendent):");
+            arr.CopyTo(arr2, 0);
+            Task2.SortByRowSum(arr2, SortOrder.Descendent);
+            PrintArray(arr2);
+
+            Console.WriteLine("Sorted by min row element(ascendent):");
+            arr.CopyTo(arr2, 0);
+            Task2.SortByRowSum(arr2, SortOrder.Ascendent);
+            PrintArray(arr2);
+
+            Console.WriteLine("Sorted by min row element(descendent):");
+            arr.CopyTo(arr2, 0);
+            Task2.SortByRowSum(arr2, SortOrder.Descendent);
+            PrintArray(arr2);
+
             Console.ReadKey();
+        }
+
+        static void PrintArray(int[][] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == null)
+                    Console.Write("null ");
+                else
+                {
+                    for (int j = 0; j < arr[i].Length; j++)
+                        Console.Write(arr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Logic/Task2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public enum SortOrder
+    {
+        Ascendent,
+        Descendent
+    }
+
+    public static class Task2
+    {
+        public static void SortByRowSum(int[][] arr, SortOrder order)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                    if (order == SortOrder.Ascendent)
+                    {
+                        if ((arr[j] == null) ||(arr[j + 1] != null && arr[j].Sum() > arr[j + 1].Sum()))
+                        {
+                            int[] temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    else
+                        if ((arr[j] == null) || (arr[j + 1] != null && arr[j].Sum() < arr[j + 1].Sum()))
+                        {
+                            int[] temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+        }
+
+        public static void SortByMaxRowElement(int[][] arr, SortOrder order)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                    if (order == SortOrder.Ascendent)
+                    {
+                        if ((arr[j] == null) || (arr[j + 1] != null && arr[j].Max() > arr[j + 1].Max()))
+                        {
+                            int[] temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    else
+                        if ((arr[j] == null) || (arr[j + 1] != null && arr[j].Max() < arr[j + 1].Max()))
+                    {
+                        int[] temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+        public static void SortByMinRowElement(int[][] arr, SortOrder order)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                    if (order == SortOrder.Ascendent)
+                    {
+                        if ((arr[j] == null) || (arr[j + 1] != null && arr[j].Min() > arr[j + 1].Min()))
+                        {
+                            int[] temp = arr[j];
+                            arr[j] = arr[j + 1];
+                            arr[j + 1] = temp;
+                        }
+                    }
+                    else
+                        if ((arr[j] == null) || (arr[j + 1] != null && arr[j].Min() < arr[j + 1].Min()))
+                    {
+                        int[] temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+    }
+}
