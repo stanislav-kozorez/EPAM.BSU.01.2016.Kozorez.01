66d762a1db234b50b6c9672d27c82cee4de8bbf6
_shark <stanislav.kozorez@gmail.com>
2016-03-08 04:00:30 +0300

    Task 1 almost complete

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logic Pow: {0} ", Task1.Root(3, 1/20.0));
+            Console.WriteLine("Math Pow: {0} ", Math.Pow(3, 20));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Logic/Task1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    public static class Task1
+    {
+        private const double ACCURACY = 0.0001;
+        public static double Root(double x, double y)
+        {
+            double Xk = x;
+            double result = x;
+            do
+            {
+                Xk = result;
+                result = 1 / y * ((y - 1) * Xk + x / (Math.Exp((y - 1) * Math.Log(Xk))));
+                
+
+            } while (Math.Abs(result - Xk) > ACCURACY);
+
+            return result;
+        }
+
+    }
+}
